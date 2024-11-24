ec7209333e90e80259e94ebde3e48472d8f8f40f
leniks <94530828+leniks@users.noreply.github.com>
2024-11-25 01:27:17 +0300

    Update workload.py

--- workload/src/models/workload.py
+++ workload/src/models/workload.py
@@ -1,27 +1,14 @@
 from sqlalchemy import Column, BigInteger, String, CheckConstraint, ForeignKey
 from sqlalchemy.orm import relationship, Mapped
-from enum import Enum
 
 from src.models import BaseWithId
 from src.models.workload_group import group_workload_association
 
 
-class WorkloadType(Enum):
-    PRACTICAL = "Практическое занятие"
-    LABORATORY = "Лабораторная работа"
-    COURSE_WORK = "Курсовая работа"
-    COURSE_PROJECT = "Курсовой проект"
-    CONSULTATION = "Консультация"
-    RATING = "Рейтинг"
-    CREDIT = "Зачёт"
-    EXAM = "Экзамен"
-    LECTURE = "Лекция"
-
-
 class Workload(BaseWithId):
     __tablename__ = 'workloads'  # Используйте согласованную конвенцию именования
 
-    type = Column(Enum(WorkloadType), nullable=False)
+    type = Column(String(255), nullable=False)
     workload = Column(BigInteger, nullable=False)
     lesson_id = Column(BigInteger, ForeignKey('lessons.id'), nullable=False)
     workload_container_id = Column(BigInteger, ForeignKey('workload_container.id'), nullable=True)
