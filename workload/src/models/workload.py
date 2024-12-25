ce62bfad94dc3cfaaa009786a7db252c28827f14
lenikss <anciborko04@mail.ru>
2024-12-25 15:46:55 +0300

    type of workload enum beningin

--- workload/src/models/workload.py
+++ workload/src/models/workload.py
@@ -1,8 +1,20 @@
-from sqlalchemy import Column, BigInteger, String, CheckConstraint, ForeignKey
+from sqlalchemy import Column, BigInteger, String, CheckConstraint, ForeignKey, Enum as SQLEnum
 from sqlalchemy.orm import relationship, Mapped
 
 from src.models import BaseWithId
 from src.models.workload_group import group_workload_association
+
+from enum import Enum
+class TypeOfWorkload(Enum):
+    PracticalLesson = 'Практическое занятие'
+    LaboratoryWork = 'Лабораторная работа'
+    CourseWork = 'Курсовая работа'
+    CourseProject = 'Курсовой проект'
+
+    Consultation = 'Консультация'
+    Rating = 'Рейтинг'
+    Credit = 'Зачёт'
+    Exam = 'Экзамен'
 
 
 class Workload(BaseWithId):
